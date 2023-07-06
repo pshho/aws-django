217a0310de4e37ee6265e6a2924f806d962b52a7
pshho <awdr498@gmail.com>
2023-07-06 23:49:57 +0900

    static

--- polls/config/settings.py
+++ polls/config/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 AUTHENTICATION_BACKENDS = (
     # 로그인 과정 처리 담당
